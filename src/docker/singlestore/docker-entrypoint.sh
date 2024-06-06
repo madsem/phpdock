c37b5656657bc472e7e1020e097ef8892155cfe3
Kirill <kirill@leif.agency>
2024-06-06 10:39:04 +0300

    added init container for singlestore

---
+++ src/docker/singlestore/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -e
+set -u
+set -o pipefail
+
+url="root:${ROOT_PASSWORD}@singlestore:9000/api/v1/query/rows"
+header="content-type: application/json"
+
+
+curl -s -XPOST -H "$header" -d "{ \"sql\": \"CREATE DATABASE IF NOT EXISTS ${DB_NAME};\" }" $url
+curl -s -XPOST -H "$header" -d "{ \"sql\": \"CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';\" }" $url
+curl -s -XPOST -H "$header" -d "{ \"sql\": \"GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';\" }" $url
+curl -s -XPOST -H "$header" -d "{ \"sql\": \"FLUSH PRIVILEGES;\" }" $url
+
+echo "Done"
